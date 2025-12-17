d36601f2e5e891e2b44baf1a1f4f42e8519e3294
Marcelo Ezequiel <marcelogustavo05@hotmail.com>
2025-12-17 20:23:55 -0300

    fix(react): usar 3 iguais é mais seguro pois não converte o tipo das variaveis.

--- projects/react/mosh-curse/src/components/ListGroup.tsx
+++ projects/react/mosh-curse/src/components/ListGroup.tsx
@@ -7,7 +7,7 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
-      {items.length == 0 && <p>No item found</p>}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
