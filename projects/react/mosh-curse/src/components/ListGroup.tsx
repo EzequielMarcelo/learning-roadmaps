0ba999a32a6899a2bed4db2709d42f67abe973fd
Marcelo Ezequiel <marcelogustavo05@hotmail.com>
2025-12-22 11:55:24 -0300

    feat(react): selecionando item ao clicar com botão direito do mouse.

--- projects/react/mosh-curse/src/components/ListGroup.tsx
+++ projects/react/mosh-curse/src/components/ListGroup.tsx
@@ -1,9 +1,10 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List</h1>
@@ -11,9 +12,15 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
