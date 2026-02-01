bce79086b882828e6323ccab7a4f5ee818e0ac6e
Marcelo Ezequiel <marcelogustavo05@hotmail.com>
2026-02-01 12:41:06 -0300

    style(frame-streaming-zmq): tipo das variáveis definido no módulo FramePublisher.

--- projects/computer-vision/frame-streaming/frame-streaming-zmq/producer/src/libs/publisher.py
+++ projects/computer-vision/frame-streaming/frame-streaming-zmq/producer/src/libs/publisher.py
@@ -1,16 +1,21 @@
 import zmq
 import cv2
+import numpy as np
 
 class FramePublisher:
     def __init__(self, addr):
         ctx = zmq.Context.instance()
-        self.socket = ctx.socket(zmq.PUB)
+        self.socket: zmq.Socket = ctx.socket(zmq.PUB)
         self.socket.bind(addr)
 
-    def send(self, frame, quality):
+    def send(self, frame: np.ndarray, time: int, quality: int):
         _, buf = cv2.imencode(
             ".jpg",
             frame,
             [int(cv2.IMWRITE_JPEG_QUALITY), quality]
         )
-        self.socket.send(buf.tobytes())
+        self.socket.send_multipart([
+            b"frame",
+            time.to_bytes(8, "big"),
+            buf.tobytes()
+            ])
